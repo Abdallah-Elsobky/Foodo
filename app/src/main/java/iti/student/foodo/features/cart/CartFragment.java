e34884f382cf761684d2b9e7e68fffb9f2095248
Abdallah Elsobky <abdullah442286@fsc.bu.edu.eg>
2026-01-30 22:45:21 +0200

    core(navigation): implement bottom navigation with root destinations

---
+++ app/src/main/java/iti/student/foodo/features/cart/CartFragment.java
@@ -0,0 +1,31 @@
+package iti.student.foodo.features.cart;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import iti.student.foodo.R;
+import iti.student.foodo.databinding.FragmentCartBinding;
+
+
+public class CartFragment extends Fragment {
+
+    FragmentCartBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentCartBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/iti/student/foodo/features/onboarding/OnboardingFragment.java
@@ -0,0 +1,40 @@
+package iti.student.foodo.features.onboarding;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import iti.student.foodo.R;
+import iti.student.foodo.databinding.FragmentOnboardingBinding;
+
+public class OnboardingFragment extends Fragment {
+
+    FragmentOnboardingBinding binding;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentOnboardingBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.onboarding.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.navigateToAuthFromOnboarding);
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
