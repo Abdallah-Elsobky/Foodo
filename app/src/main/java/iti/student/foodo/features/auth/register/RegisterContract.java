d29c590da98df84201f73f1de2736ec02c8d5545
Abdallah Elsobky <abdullah442286@fsc.bu.edu.eg>
2026-02-04 00:32:46 +0200

    feature(auth): implement signup with firebase

---
+++ app/src/main/java/iti/student/foodo/features/auth/register/RegisterContract.java
@@ -0,0 +1,16 @@
+package iti.student.foodo.features.auth.register;
+
+import iti.student.foodo.core.base.BasePresenter;
+import iti.student.foodo.core.base.BaseView;
+
+public interface RegisterContract {
+    interface View extends BaseView {
+        void onRegisterSuccess();
+
+        void onRegisterFailure(String message);
+    }
+
+    interface Presenter extends BasePresenter<View> {
+        void register(String email, String password);
+    }
+}
